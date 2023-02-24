d3244468ff452efbeede34ead918a9d5c5aba8a9
Satanaelcode <satanaelcoop@proton.me>
2023-02-24 03:13:59 +0100

    filter out the common banned Unicode symbols

--- pyssword.py
+++ pyssword.py
@@ -1,8 +1,6 @@
 import string
 import random
-import subprocess
-from subprocess import CompletedProcess
-from typing import Any
+import requests
 
 # Printing The Logo
 
@@ -14,7 +12,7 @@ print(""""
    \u001b[36m#\u001b[0m\u001b[33m#\u001b[0m\u001b[33m#\u001b[0m\u001b[33m#\u001b[0m\u001b[36m#\u001b[0m\u001b[36m#\u001b[0m\u001b[36m#\u001b[0m\u001b[36m#\u001b[0m\u001b[33m#\u001b[0m\u001b[33m#\u001b[0m\u001b[33m#\u001b[0m\u001b[36m#\u001b[0m  |_|   |__/ /__//__/  \_/\_/ \___/|_|  \__/_|
    \u001b[36m#\u001b[0m\u001b[36m#\u001b[0m\u001b[33m#\u001b[0m\u001b[33m#\u001b[0m\u001b[33m#\u001b[0m\u001b[33m#\u001b[0m\u001b[33m#\u001b[0m\u001b[36m#\u001b[0m\u001b[33m#\u001b[0m\u001b[33m#\u001b[0m\u001b[36m#\u001b[0m\u001b[36m#\u001b[0m
     \u001b[36m#\u001b[0m\u001b[36m#\u001b[0m\u001b[33m#\u001b[0m\u001b[33m#\u001b[0m\u001b[33m#\u001b[0m\u001b[33m#\u001b[0m\u001b[33m#\u001b[0m\u001b[33m#\u001b[0m\u001b[36m#\u001b[0m\u001b[36m#\u001b[0m
-     \u001b[36m#\u001b[0m\u001b[36m#\u001b[0m\u001b[36m#\u001b[0m\u001b[36m#\u001b[0m\u001b[36m#\u001b[0m\u001b[36m#\u001b[0m\u001b[36m#\u001b[0m\u001b[36m#\u001b[0m
+     \u001b[36m#\u001b[0m\u001b[36m#\u001b[0m\u001b[36m#\u001b[0m\u001b[36m#\u001b[0m\u001b[36m#\u001b[0m\u001b[36m#\u001b[0m\u001b[36m#\u001b[0m\u001b[36m#\u001b[0m 
 """)
 
 # Asking User for Password Length
@@ -22,12 +20,14 @@ print(' <Number> for length. \n credits for Credits. \n check for checking the v
 lang = input()
 
 # check for version number and format version into a string
-version = subprocess.run('curl https://pastebin.com/raw/8x7dtvaS', capture_output=True).stdout.decode()
+
+headers = {'User-Agent': 'curl/7.64.1'}
+version = requests.post('https://pastebin.com/raw/8x7dtvaS', headers=headers)
 
 
 # define generate_password function
 def generate_password(length):
-    password = ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(length))
+    password = ''.join(random.choice(string.ascii_letters+string.digits+str('?')+str('!')+str('*')+str('#')+str('&')) for _ in range(length))
     return password
 
 
@@ -35,15 +35,18 @@ def generate_password(length):
 if lang == 'credits':
 
     # noinspection PyTypeChecker
-    print('\n CREDITS: \n Origin Code: Sheesha \n Optimizer: EuGaming \n Version: ' + version + '\n Discord: https://discord.gg/cQAk2etuY3 \n')
+    print('\n CREDITS: \n Origin Code: Sheesha \n Optimizer: EuGaming \n Version: ' + version.content + '\n Discord: https://discord.gg/cQAk2etuY3 \n')
 
 elif lang == 'check':
 
-    if version == '1.4.2':
+    if version.content == '1.4.3':
         print('\n \033[1;32;40m up to date!')
     else:
 
-        print('\n \033[31m Update available! \n you a on 1.4 but the newest version is ' + version)
+        versionraw = version.content
+        print('\n \033[31m Update available! \n you a on 1.4.2 but the newest version is ' + str(versionraw))
 else:
     # Print finished Password if lang was a number
     print('\n \033[0m Here is the Password: \n \n' + generate_password(int(lang)) + '\n')
+
+
